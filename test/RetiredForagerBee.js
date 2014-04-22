d9616204ed56fd9ceeb7795195f01ff04b7b6566
Phillip Alexander <git@phillipalexander.io>
2014-04-21 17:36:28 -0700

    Fix test names

--- test/RetiredForagerBee.js
+++ test/RetiredForagerBee.js
@@ -3,51 +3,51 @@ describe('RetiredForagerBee class functionality', function() {
   verifyClass(RetiredForagerBee).followsPattern('pseudoclassical', {}, false);
 
   beforeEach(function() {
-    retiredForrager = new RetiredForagerBee();
+    retiredForager = new RetiredForagerBee();
   });
 
   /*  Overwrite methods from superclass  */
   
   it('should have an `age` property that is set to `40`', function() {
-    expect(retiredForrager.age).to.equal(40);
+    expect(retiredForager.age).to.equal(40);
   });
 
   it('should have a `job` property that is set to `gamble`', function() {
-    expect(retiredForrager.job).to.equal('gamble');
+    expect(retiredForager.job).to.equal('gamble');
   });
 
   it('should have a `canFly` property that is set to `false`', function() {
-    expect(retiredForrager.canFly).to.equal(false);
+    expect(retiredForager.canFly).to.equal(false);
   });
 
   it('should have a `color` property that is set to `grey`', function() {
-    expect(retiredForrager.color).to.equal('grey');
+    expect(retiredForager.color).to.equal('grey');
   });
 
   it('should have a `forrage` method that returns "I am too old, let me play cards instead"', function() {
-    expect(retiredForrager.forrage()).to.equal('I am too old, let me play cards instead');
+    expect(retiredForager.forrage()).to.equal('I am too old, let me play cards instead');
   });
 
   /*  Inhereted from superclass  */
 
   it('should have a `food` property that is inhereted from grub', function() {
-    expect(retiredForrager.food).to.equal('jelly');
+    expect(retiredForager.food).to.equal('jelly');
   });
 
   it('should have an `eat` method that is inhereted from grub', function() {
-    expect(retiredForrager.eat).to.be.a('function');
+    expect(retiredForager.eat).to.be.a('function');
   });
 
   it('should have a `treasureChest` property inhereted from forragerBee that is set to an empty array `[]`', function() {
-    expect(retiredForrager.treasureChest).to.be.a('array');
+    expect(retiredForager.treasureChest).to.be.a('array');
   });
 
   /*  New methods and properties  */
 
   it('should have an always winning `gamble` method that allows the bee to add a `treasure` to the `treasureChest`', function() {
-    retiredForrager.gamble();
-    retiredForrager.gamble();
-    expect(retiredForrager.treasureChest).to.have.length(2);
+    retiredForager.gamble();
+    retiredForager.gamble();
+    expect(retiredForager.treasureChest).to.have.length(2);
   });
 
 });
