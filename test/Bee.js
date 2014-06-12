7527ea7d65cbab7906afccfa680e64f98eb1bc0e
Phillip Alexander <git@phillipalexander.io>
2014-06-11 20:52:03 -0700

    Fix case of test

--- test/Bee.js
+++ test/Bee.js
@@ -1,5 +1,5 @@
 describe('Bee class functionality', function() {
-  
+
   verifyClass(Bee).followsPattern('pseudoclassical', {}, false);
 
   beforeEach(function() {
@@ -29,7 +29,7 @@ describe('Bee class functionality', function() {
   /*  New methods and properties  */
 
   it('should have a `job` property that is set to `keep on growing`', function() {
-    expect(bee.job).to.equal('keep on growing');
+    expect(bee.job).to.equal('Keep on growing');
   });
 
 });
