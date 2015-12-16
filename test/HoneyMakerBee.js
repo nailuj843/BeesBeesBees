db631a033d9fd0677e66a2135112ae2f7719fe5d
Dan Thareja <danthareja@gmail.com>
2015-12-15 19:21:00 -0800

    Refactor tests to use ES6

--- test/HoneyMakerBee.js
+++ test/HoneyMakerBee.js
@@ -1,44 +1,42 @@
 import { expect } from 'chai';
 import HoneyMakerBee from '../src/HoneyMakerBee';
 
-describe('HoneyMakerBee class functionality', function() {
+describe('HoneyMakerBee class functionality', () => {
   let honeyBee;
 
-  beforeEach(function() {
-    honeyBee = new HoneyMakerBee();
-  });
+  beforeEach(() => honeyBee = new HoneyMakerBee());
 
   /*  Overwrite methods from superclass  */
 
-  it('should have an age property that is set to `10`', function() {
+  it('should have an age property that is set to `10`', () => {
     expect(honeyBee.age).to.equal(10);
   });
 
-  it('should have a job property that is set to `make honey`', function() {
+  it('should have a job property that is set to `make honey`', () => {
     expect(honeyBee.job).to.equal('make honey')
   });
 
   /*  Inherited from superclass  */
 
-  it('should have a color property inherited from `bee` that is set to `yellow`', function() {
+  it('should have a color property inherited from `bee` that is set to `yellow`', () => {
     expect(honeyBee.color).to.equal('yellow');
   });
 
-  it('should have a food property that is inherited from grub', function() {
+  it('should have a food property that is inherited from grub', () => {
     expect(honeyBee.food).to.equal('jelly');
   });
 
-  it('should have an eat method that is inherited from grub', function() {
+  it('should have an eat method that is inherited from grub', () => {
     expect(honeyBee.eat).to.be.a('function');
   });
 
   /*  New methods and properties  */
 
-  it('should have a `honeyPot` property that is set to `0`', function() {
+  it('should have a `honeyPot` property that is set to `0`', () => {
     expect(honeyBee.honeyPot).to.equal(0);
   });
 
-  it('should have a `makeHoney` method that adds `1` to that honeyBee\'s honeyPot', function() {
+  it('should have a `makeHoney` method that adds `1` to that honeyBee\'s honeyPot', () => {
     expect(honeyBee.makeHoney).to.be.a('function');
     honeyBee.makeHoney();
     expect(honeyBee.honeyPot).to.equal(1);
@@ -46,7 +44,7 @@ describe('HoneyMakerBee class functionality', function() {
     expect(honeyBee.honeyPot).to.equal(2);
   });
 
-  it('should have a `giveHoney` method that subtracts `1` from that honeyBee\'s honeyPot', function() {
+  it('should have a `giveHoney` method that subtracts `1` from that honeyBee\'s honeyPot', () => {
     expect(honeyBee.giveHoney).to.be.a('function');
     honeyBee.makeHoney();
     honeyBee.makeHoney();
