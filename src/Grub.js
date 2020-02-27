cd398d73e86ccc2cd4816430e87473fc5cfbafc4
Brandon Campbell-Kearns <bk@brandonkearns.com>
2020-02-27 09:49:07 -1000

    Clears Grub class

--- src/Grub.js
+++ src/Grub.js
@@ -1,13 +1,9 @@
 class Grub {
   /* START SOLUTION */
   constructor() {
-    this.age = 0;
-    this.color = 'pink';
-    this.food = 'jelly';
+    
   }
 
-  eat() {
-    return 'Mmmmmmmmm ' + this.food;
-  }
+  
   /* END SOLUTION */
 };
