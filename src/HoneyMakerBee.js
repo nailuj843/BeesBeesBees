f17b37b4b69c7b50d8796aac9ce820e149c0debf
Brandon Campbell-Kearns <bk@brandonkearns.com>
2020-02-27 10:18:50 -1000

    Clears all the Bee classes

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,23 +1,3 @@
 /* START SOLUTION */
-class HoneyMakerBee extends Bee {
-  constructor() {
-    super();
 
-    this.age = 10;
-    this.job = 'make honey';
-    this.honeyPot = 0;
-  }
-
-  makeHoney() {
-    this.honeyPot++;
-  }
-
-  giveHoney() {
-    this.honeyPot--;
-  }
-};
-/* ELSE
-class HoneyMakerBee {
-  // TODO..
-};
-END SOLUTION */
+/* END SOLUTION */
