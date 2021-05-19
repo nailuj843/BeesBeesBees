69ed56fb6ba1952508f0c4aab45542d9612723ea
nailuj843 <nailuj843@yahoo.com>
2021-05-19 12:32:56 -0400

    pushed to repository

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
 /* START SOLUTION */
+class ForagerBee extends Bee{
+    constructor(){
+        super()
+        this.age = 10
+        this.job = 'find pollen'
+        this.color = 'yellow'
+        this.canFly = true
+        this.treasureChest = []
+    }
 
+    forage(treasure){
+        this.treasureChest.push(treasure)
+    }
+
+}
 /* END SOLUTION */
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 /* START SOLUTION */
+class HoneyMakerBee extends Bee{
+    constructor(){
+        super()
+        this.age = 10
+        this.job = 'make honey'
+        this.color = 'yellow'
+        this.honeyPot = 0
+    }
 
+    makeHoney(){
+        this.honeyPot ++
+    }
+
+    giveHoney(){
+        this.honeyPot--
+    }
+}
 /* END SOLUTION */
